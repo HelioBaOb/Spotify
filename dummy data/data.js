97d25d845fa2abffb9afd38e8cfd2f7d0ea53e31
Alexis <gpalexis18@gmail.com>
2021-10-30 18:46:47 -0500

    +30 canciones dummy data

--- dummy data/data.js
+++ dummy data/data.js
@@ -124,7 +124,46 @@ export const PLAYLISTS =[
         duration: "1 hr 28 min",
         creator: "Helio Barriga Obregon",
         songs:[
-
+            {
+                id: 1,
+                name:"When The Sun Goes Down",
+                imgUrl:'https://www.nacionrock.com/wp-content/uploads/arctic.jpg',
+                duration: "3 min 22 seg",
+                artist: "Arctic Monkeys",
+                lyrics: "So who's that girl there? I wonder what went wrong So that she had to roam the streets She don't do major credit cards I doubt she does receipts It's all not quite legitimate And what a scummy man Just give him half a chance I bet he'll rob you if he can Can see it in his eyes, yeah That he's got a driving ban Amongst some other offences",
+              },
+              {
+                id: 2,
+                name:"Fluorecent Adolescent",
+                imgUrl:'https://m.media-amazon.com/images/I/81jUUHuNvEL._SL1425_.jpg',
+                duration: "3 min 4 seg",
+                artist: "Arctic Monkeys",
+                lyrics: "You used to get it in your fishnets Now you only get it in your night dress Discarded all the naughty nights for niceness Landed in a very common crisis Everything's in order in a black hole Nothing seems as pretty as the past, though That Bloody Mary's lacking a Tabasco Remember when you used to be a rascal?  Oh, that boy's a slag The best you ever had The best you ever had Is just a memory and those dreams Weren't as daft as they seemed Not as daft as they seemed My love, when you dreamed them up",
+              },
+              {
+                id: 3,
+                name:"R U Mine?",
+                imgUrl:'https://upload.wikimedia.org/wikipedia/commons/e/e7/%22AM%22_%28Arctic_Monkeys%29.jpg',
+                duration: "3 min 22 seg",
+                artist: "Arctic Monkeys",
+                lyrics: "I'm a puppet on a string Tracy Island, time-travelin' diamond cutter-shaped heartaches Come to find you four in some velvet mornin' years too late She's a silver linin', lone ranger ridin' through an open space In my mind, when she's not right there beside me I go crazy 'cause here isn't where I wanna be And satisfaction feels like a distant memory And I can't help myself All I wanna ever say is, Are you mine? Well, are you mine? Are you mine? Are you mine? Oh, ah",
+              },
+              {
+                id: 4,
+                name:"I Bet You Look Good on the Dancefloor",
+                imgUrl:'https://www.nacionrock.com/wp-content/uploads/arctic.jpg',
+                duration: "2 min 54 seg",
+                artist: "Arctic Monkeys",
+                lyrics: "Stop making the eyes at me I'll stop making the eyes at you What it is that surprises me Is that I don't really want you to And your shoulders are frozen (cold as the night) Oh, but you're an explosion (you're dynamite) Your name isn't Rio, but I don't care for sand And lighting the fuse might result in a bang, b-b-bang-oh",
+              },
+              {
+                id: 5,
+                name:"Mardy Bum",
+                imgUrl:'https://www.nacionrock.com/wp-content/uploads/arctic.jpg',
+                duration: "2 min 55 seg",
+                artist: "Arctic Monkeys",
+                lyrics: "Well, now then, mardy bum I've seen your frown and it's like looking down The barrel of a gun And it goes off And out come all these words Oh, there's a very pleasant side to you A side I much prefer It's one that laughs and jokes around Remember cuddles in the kitchen, yeah To get things off the ground And it was up, up and away Oh, but it's right hard to remember that On a day like today When you're all argumentative And you've got the face on",
+              },
         ]
     },
     {
@@ -144,7 +183,46 @@ export const PLAYLISTS =[
         imgUrl:'https://i.scdn.co/image/ab67706f00000003723228643ca262e4d71cae24',
 	    duration: "4h 13m",
         songs:[
-
+            {
+                id: 1,
+                name:"The Spins",
+                imgUrl:'https://www.mor.bo/wp-content/uploads/2020/05/kids.jpg',
+                duration: "3 min 16 seg",
+                artist: "Mac Miller",
+                lyrics: "Dope shit like that Jerm Oh hi Welcome back to K.I.D.S Umm, follow your dreams Yeah! Wanna get a mansion A jacuzzi, a theater to watch my movies A couple whips and lots of fancy things The kids they call the Goonies I see the future, crystal ball Mirror, mirror hangin' on the wall Who the flyest white boy of them dem all? Got your girlfriend screenin' all the calls",
+              },
+              {
+                id: 2,
+                name:"Kool Aid & Frozen Pizza",
+                imgUrl:'https://www.mor.bo/wp-content/uploads/2020/05/kids.jpg',
+                duration: "2 min 38 seg",
+                artist: "Mac Miller",
+                lyrics: "Kids Hey Kool-Aid and frozen pizza It's a work of art, I ain't talking Mona Lisa Dream of rocking crowds, see me filling up arenas This weed got me laughing like a damn hyena (ha) Yeah, I live a life pretty similar to yours Used to go to school, hang with friends and play sports Every single summer taking trips to the shore And that was all gravy but I knew I wanted more",
+              },
+              {
+                id: 3,
+                name:"Right",
+                imgUrl:'https://www.mondosonoro.com/wp-content/uploads/2020/01/mac-miller-circles.jpg',
+                duration: "4 min 48 seg",
+                artist: "Mac Miller",
+                lyrics: "Yeah, it's been a while without your face But I saw your picture on the wall the other day Too much distance, too much space You need to come back home or run along, yeah I'm waitin' for the light to change You ask me how I've been, well I'm good, I can't complain Times get harder, things get strained All I know, I don't want you gone",
+              },
+              {
+                id: 4,
+                name:"Good News",
+                imgUrl:'https://www.mondosonoro.com/wp-content/uploads/2020/01/mac-miller-circles.jpg',
+                duration: "5 min 42 seg",
+                artist: "Mac Miller",
+                lyrics: "Well this is what it look like right before you fall Stumblin' around, you've been guessin' your direction Next step you can't see at all And I don't have a name, I don't have a name, no Who am I to blame, who am I to blame though? And I cannot be changed, I cannot be changed, no Trust me, I've tried I just end up right at the start of the line Drawin' circles",
+              },
+              {
+                id: 5,
+                name:"Time",
+                imgUrl:'https://media.socastsrm.com/wordpress/wp-content/blogs.dir/2162/files/2019/06/62205725_310411836503608_4657536626805029247_n.jpg',
+                duration: "3 min 30 seg",
+                artist: "Mac Miller",
+                lyrics: "I gotta take some time to grow But I don't wanna let you go How did we get too comfortable? I gotta take some time to grow But without you, I'm miserable I think I got too comfortable We could take it to the day that I saw you On your way to the top Or we could take it to the time that I saw You lose your mind and give up Everybody gets down on luck Every time you fall I try to pick you up But I need reciprocation",
+              },
         ]
     },
     {
@@ -164,7 +242,46 @@ export const PLAYLISTS =[
         imgUrl:'https://cdns-images.dzcdn.net/images/cover/b96a3116af78b8c516f18ba367e36bc0/500x500.jpg',
 	    duration: "21h 32m",
         songs:[
-
+            {
+                id: 1,
+                name:"Awaken",
+                imgUrl:'https://cdns-images.dzcdn.net/images/cover/b96a3116af78b8c516f18ba367e36bc0/500x500.jpg',
+                duration: "3 min 18 seg",
+                artist: "League of Legends",
+                lyrics: "The night beckons while you dream A life never lives in peace As you stand upon the edge Woven by a single thread And fate may fall down upon you While the devil is knocking Right at your door So all you restless Each night you hear the drums of war Awaken, awaken The voice begins to call you while you hunger A taste of destiny you're searching for Awaken, awaken The fight is at your door, take up the cause",
+              },
+              {
+                id: 2,
+                name:"Snow (Hey Oh)",
+                imgUrl:'https://cdns-images.dzcdn.net/images/cover/7ac261fcdc9ae04f827e62e33077c6c3/264x264.jpg',
+                duration: "5 min 35 seg",
+                artist: "Red Hot Chilli Peppers",
+                lyrics: "Come to decide that the things that I tried Were in my life just to get high on When I sit alone Come get a little known But I need more than myself this time Step from the road to the sea to the sky And I do believe that we rely on When I lay it on Come get to play it on All my life to sacrifice",
+              },
+              {
+                id: 3,
+                name:"Winter",
+                imgUrl:'https://i.pinimg.com/originals/e5/12/35/e51235b2b84d98edf02e675ec17e8a46.jpg',
+                duration: "4 min 2 seg",
+                artist: "Khalid",
+                lyrics: "II lost my heart in the nighttime She left me out in the cold She left me broken and weary Drunk off the lies she told I fell in love in the morning But when the day gets cold I'm back to life in my lonely City of El Paso",
+              },
+              {
+                id: 4,
+                name:"Skimming Stones",
+                imgUrl:'https://m.media-amazon.com/images/I/81c4eUkYAQL._SL1500_.jpg',
+                duration: "3 min 36 seg",
+                artist: "Seafret",
+                lyrics: "I'm leavin again Hopin' time doesn't mend Don't either of us know what is happenin' Theres no right time to go I will tighten this hold You know when I walk away I'll be back again If there was a place I could take ya I'd keep you safe, far away from the world Like a skimmin' stone waiting to be thrown Back to you",
+              },
+              {
+                id: 5,
+                name:"Oceans",
+                imgUrl:'https://img.discogs.com/obDk-GqUX2h_B5fXMhgWoZA-95A=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-12922991-1544606338-6439.jpeg.jpg',
+                duration: "3 min 36 seg",
+                artist: "Seafret",
+                lyrics: "I want you Yeah I want you And nothing comes close To the way that I need you I wish I can feel your skin And I want you From somewhere within It feels like there's oceans Between me and you once again We hide our emotions Under the surface and tryin' to pretend But it feels like there's oceans Between you and me",
+              },
         ]
     },
     {
@@ -174,7 +291,46 @@ export const PLAYLISTS =[
         imgUrl:'https://i.pinimg.com/736x/ff/0d/0b/ff0d0b6ccdafa0f7818077a78af52615.jpg',
 	    duration: "2h 31m",
         songs:[
-
+            {
+                id: 1,
+                name:"Choke",
+                imgUrl:'https://direct.rhapsody.com/imageserver/images/alb.325350587/600x600.jpg',
+                duration: "3 min 16 seg",
+                artist: "I DON'T KNOW HOW BUT THEY FOUND ME",
+                lyrics: "Stop, drop And drag me into place And lock the fire escapes I'll break your pretty face (Nyeah, nyeah) Oh, you clever little things The sycophantic teens What a precious basket case (Nyeah, nyeah)",
+              },
+              {
+                id: 2,
+                name:"Do it All The Time",
+                imgUrl:'https://direct.rhapsody.com/imageserver/images/alb.325350275/600x600.jpg',
+                duration: "2 min 48 seg",
+                artist: "I DON'T KNOW HOW BUT THEY FOUND ME",
+                lyrics: "Ha! We're taking over the world One kiss at a time And then I'm taking your girl And I'm making her mine No reason why I'm only doing anything I want to do Because I do it all the time We're taking over the world A little victim-less crime And when I'm taking your innocence I'll be corrupting your mind",
+              },
+              {
+                id: 3,
+                name:"Young & Alive",
+                imgUrl:'https://m.media-amazon.com/images/I/811lQG22CAL._SS500_.jpg',
+                duration: "2 min 40 seg",
+                artist: "Bazzi",
+                lyrics: "Ridin' my bike like I'm too young to drive Careless and free like it's 2005 Had to chill the fuck out, take a trip outside To have fun with my friends like I'm young and alive So can I get your AOL? Hit you lot like, LOL (L) This feel like our high school days Runnin' 'round at football games (games) We ain't got a fuck or nothing, we can hold hands or somethin'",
+              },
+              {
+                id: 4,
+                name:"Rollin",
+                imgUrl:'https://img.discogs.com/YLvmlbY7lejeIxt8w8rraIJvK7k=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-10291060-1494760396-5981.jpeg.jpg',
+                duration: "4 min 33 seg",
+                artist: "Calvin Harris",
+                lyrics: "I've been rollin' on the freeway I've been riding 85 I've been thinking way too much And I'm way too gone to drive I got anger in my chest I got millions on my mind And you didn't fit the picture So I guess you weren't the vibe I've been rollin' on the freeway I've been riding 85 I've been thinking way too much And I'm way too gone to drive I got anger in my chest I got millions on my mind And you didn't fit the picture So I guess you weren't the vibe",
+              },
+              {
+                id: 5,
+                name:"Freaking Out the Neighborhood",
+                imgUrl:'https://cdns-images.dzcdn.net/images/cover/48dd98d88f1af797d65faf7f3e4beef7/350x350.jpg',
+                duration: "2 min 54 seg",
+                artist: "Mac DeMarco",
+                lyrics: "Sorry, mama There are times I get carried away But please don't worry Next time I'm home, I'll still be the same And I know it's no fun When your first son Gets up to no good Starts freaking out the neighborhood Really, I'm fine Never been better, got no job on the line Sincerely, don't worry Same old boy that you hoped you would find",
+              },
         ]
     },
     {
@@ -184,7 +340,46 @@ export const PLAYLISTS =[
         imgUrl:'https://steamuserimages-a.akamaihd.net/ugc/1638705743367141279/E0F5142714F86C00E413C08D8FC95159DD10D796/?imw=637&imh=358&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true',
 	    duration: "1h 24m",
         songs:[
-            
+            {
+                id: 1,
+                name:"Binary Sunset",
+                imgUrl:'https://i.scdn.co/image/ab67616d0000b273690bb7b4cc194a09858293e3',
+                duration: "1 min 4 seg",
+                artist: "Closed on Sunday",
+                lyrics: "---",
+              },
+              {
+                id: 2,
+                name:"Cantina Band Theme",
+                imgUrl:'https://m.media-amazon.com/images/I/71m06WI8+8S._SS500_.jpg',
+                duration: "1 min 11 seg",
+                artist: "CFC",
+                lyrics: "---",
+              },
+              {
+                id: 3,
+                name:"the droid invasion",
+                imgUrl:'https://i.scdn.co/image/ab67616d0000b273431b59af7c47935fe57ca375',
+                duration: "1 min 15 seg",
+                artist: "Closed on Sunday",
+                lyrics: "---",
+              },
+              {
+                id: 4,
+                name:"the mandalorian",
+                imgUrl:'https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/ee/b5/ab/eeb5ab4a-d4e7-1b35-804d-e7880da70914/artwork.jpg/400x400cc.jpg',
+                duration: "1 min 40 seg",
+                artist: "Closed on Sunday",
+                lyrics: "---",
+              },
+              {
+                id: 5,
+                name:"rescue of the princess",
+                imgUrl:'https://m.media-amazon.com/images/I/51qVh9mmoUS._SS500_.jpg',
+                duration: "1 min 30 seg",
+                artist: "Closed on Sunday",
+                lyrics: "---",
+              },
         ]
     },
     {
@@ -194,7 +389,46 @@ export const PLAYLISTS =[
         imgUrl:'https://i.scdn.co/image/ab67616d0000b27304db0e3bcd166c1d6cfd81f9',
 	    duration: "42h 58m",
         songs:[
-            
+            {
+                id: 1,
+                name:"Thunderstruck",
+                imgUrl:'https://m.media-amazon.com/images/I/81qnqT8Zx-L._SL1500_.jpg',
+                duration: "4 min 45 seg",
+                artist: "AC/DC",
+                lyrics: "Thunder, thunder, thunder, thunder Thunder, thunder, thunder, thunder Thunder, thunder I was caught In the middle of a railroad track (thunder) I looked 'round And I knew there was no turning back (thunder) My mind raced And I thought, what could I do? (Thunder) And I knew There was no help, no help from you (thunder)",
+              },
+              {
+                id: 2,
+                name:"Back in Black",
+                imgUrl:'https://upload.wikimedia.org/wikipedia/commons/9/92/ACDC_Back_in_Black.png',
+                duration: "4 min 15 seg",
+                artist: "AC/DC",
+                lyrics: "Back in black I hit the sack I've been too long I'm glad to be back. Yes, I'm Let loose From the noose That's kept me hanging about I keep lookin' at the sky 'Cause it's gettin' me high Forget the hearse cause I'll never die I got nine lives Cat's eyes Abusin' every one of them and running wild",
+              },
+              {
+                id: 3,
+                name:"Highway to Hell",
+                imgUrl:'https://m.media-amazon.com/images/I/81k-3sh8OhL._SL1500_.jpg',
+                duration: "3 min 28 seg",
+                artist: "AC/DC",
+                lyrics: "Livin' easy Lovin' free Season ticket on a one way ride Askin' nothin' Leave me be Takin' everythin' in my stride Don't need reason Don't need rhyme Ain't nothin' that I'd rather do Goin' down Party time My friends are gonna be there too I'm on the highway to hell On the highway to hell Highway to hell I'm on the highway to hell",
+              },
+              {
+                id: 4,
+                name:"If You Want Blood (You've got it)",
+                imgUrl:'https://m.media-amazon.com/images/I/71vsHNHr-KL._SL1500_.jpg',
+                duration: "4 min 34 seg",
+                artist: "AC/DC",
+                lyrics: "It's criminal There ought to be a law Criminal There ought to be a whole lot more You get nothin' for nothin' Tell me who can you trust We got what you want And you got the lust If you want blood, you got it If you want blood, you got it Blood on the streets Blood on the rocks Blood in the gutter Every last drop You want blood You got it",
+              },
+              {
+                id: 5,
+                name:"Shoot to Thrill",
+                imgUrl:'https://upload.wikimedia.org/wikipedia/commons/9/92/ACDC_Back_in_Black.png',
+                duration: "5 min 17 seg",
+                artist: "AC/DC",
+                lyrics: "All you women who want a man of the street But don't know which way you wanna turn Just keep a-comin', and put your hand out to me 'Cause I'm the one who's gonna make you burn I'm gonna take you down Oh, down, down, down So, don't you fool around I'm gonna pull it, pull it, pull the trigger",
+              },
         ]
     },
     {
