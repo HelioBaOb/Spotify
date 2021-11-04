b721b009410ddc7226c242cebd27a75cb8c23daa
Helio Barriga <hbarriga16@alumnos.uaq.mx>
2021-11-04 17:52:48 -0600

    Fuentes y librerías nuevas
    -Se agregaron los archivos de las fuentes
    -Se modifico el archivo app.js para poder agregar nuevas fuentes.
    -Se instalaron 2 librerías: expo-font y expo-app-loading

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native'; 
 import { StackMain } from './navigation/MainNavigator';
-import React from 'react';
+import React, {useState} from 'react';
+import AppLoading from "expo-app-loading";
+import * as Font from 'expo-font';
 import { createStore, combineReducers, applyMiddleware} from 'redux';
 import ReduxThunk from 'redux-thunk';
 import { Provider } from 'react-redux';
@@ -16,6 +18,32 @@ const store = createStore(
 )
 
 export default function App() {
+
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+    const fetchFonts = async () =>{
+
+        return Font.loadAsync({
+            'circular-black': require('./assets/fonts/CircularStd-Black.ttf'),
+            'circular-bold': require('./assets/fonts/CircularStd-Bold.ttf'),
+            'circular-italic': require('./assets/fonts/CircularStd-BlackItalic.ttf'),
+            'circular-book': require('./assets/fonts/CircularStd-Book.ttf'),
+            'circular-boldItalic': require('./assets/fonts/CircularStd-BoldItalic.ttf')
+        })
+    }
+
+    if (!fontLoaded) {
+      return (
+          <AppLoading
+              startAsync={fetchFonts}
+              onFinish={() => {
+                  setFontLoaded(true);
+              }}
+              onError={console.warn}
+          />
+      );
+  }
+
   return (
     <NavigationContainer>
         <StackMain/>
